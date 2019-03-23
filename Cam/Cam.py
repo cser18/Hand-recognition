5662db2c0a64f41b4e9f81631c6651a6d683f9f6
cser18 <39697215+cser18@users.noreply.github.com>
2019-03-23 16:06:42 +0800

    加入双边滤波

--- Cam/Cam.py
+++ Cam/Cam.py
@@ -3,19 +3,21 @@ import cv2
 #添加模块和矩阵模块
 import numpy as np
 
+
 cap=cv2.VideoCapture(0)
 # 打开摄像头，若打开本地视频，同opencv一样，只需将０换成("×××.avi")
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))  # 定义结构元素
 # cap.set(cv2.CV_CAP_PROP_FPS, 30)
 cap.set(cv2.CAP_PROP_FPS,30)
-while(1):    # get a frame   
+while(1):    # get a frame
     ret, frame = cap.read()    # show a frame
     [H,W,C] = frame.shape
     frame = cv2.flip(frame, 1, dst=None)  # 水平镜像
     frame = cv2.rectangle(frame,(W - 250,H - 80),(W - 150,H - 200),(255, 0, 0), 2)
     frame_data = np.array(frame)
     frame_data = frame_data[H - 200 : H - 80, W - 250:W - 150]
-    # frame_data = cv2.blur(frame_data,(2,2))
+    # frame_data = cv2.medianBlur(frame_data, 3) 中值滤波
+    frame_data = cv2.bilateralFilter(frame_data,5,15,15) #双边滤波
     frame_data = cv2.cvtColor(frame_data, cv2.COLOR_BGR2HSV_FULL)
     [H,S,V] = cv2.split(frame_data)
     # frame_data = cv2.morphologyEx(frame_data, cv2.MORPH_OPEN, kernel)
@@ -23,8 +25,8 @@ while(1):    # get a frame
     # ret, frame_data = cv2.threshold(frame_data, 100, 255, cv2.THRESH_BINARY_INV)
     [th,tw,tc] = frame_data.shape
     H = np.array(H)
-    S = np.array(S)
-    V = np.array(V)
+    S = np.array(S)  # 饱和度
+    V = np.array(V)  # 明度
     for i  in range(th):
         for j in range(tw):
             hVal = H[i,j]
@@ -34,6 +36,7 @@ while(1):    # get a frame
                 H[i,j] = 255
             else:
                 H[i,j] = 0
+
     dit = cv2.dilate(H,kernel)
     dit = cv2.dilate(dit,kernel)
     erode = cv2.erode(H,kernel)
@@ -51,7 +54,6 @@ while(1):    # get a frame
         break
     elif key == ord('s'):
         cv2.imwrite("1.jpg",H)
-
 cap.release()
 cv2.destroyAllWindows()
 #释放并销毁窗口
